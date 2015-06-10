23383356042c7103d98ebeee9dac15255bd6823d
VinceQ33 <vinceq33@gmail.com>
2015-06-10 11:25:03 +0200

    Ejercicio módulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -30,7 +30,13 @@ var resultado = 'Incorrecto';
 
 //GET /quizes
 exports.index=function(req,res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
-	}).catch(function(error){ next(error); });
+	if (req.query.search === undefined) {
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index', { quizes: quizes });
+		}).catch(function(error) { next(error); });
+	} else {
+		models.Quiz.findAll({where: ["pregunta like ?", "%"+req.query.search.replace("+","%")+"%"]}).then(function(quizes){
+			res.render('quizes/index', { quizes: quizes });
+		}).catch(function(error) { next(error); });
+	}
 };
