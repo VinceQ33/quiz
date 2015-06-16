7ec1ea507e1f6673c5e13e5e6e008695671e23e3
VinceQ33 <vinceq33@gmail.com>
2015-06-16 09:49:19 +0200

    Ejercicio Módulo 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,7 +44,7 @@ exports.index=function(req,res){
 //GET /quizes/new
 exports.new = function(req, res){
 	var quiz = models.Quiz.build(//crea objeto quiz
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		{pregunta: "Pregunta", respuesta: "Respuesta", tema: "Otro"}
 	);
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -53,14 +53,14 @@ exports.new = function(req, res){
 exports.create = function(req, res){
 var quiz = models.Quiz.build( req.body.quiz );
 
-var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+var errors = quiz.validate();//ya que el objeto errors no tiene then(
 if (errors){
 		var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
 		for (var prop in errors) errores[i++]={message: errors[prop]};
 		res.render('quizes/new', {quiz: quiz, errors: errores});
 	} else {
 		quiz // save: guarda en DB campos pregunta y respuesta de quiz
-		.save({fields: ["pregunta", "respuesta"]})
+		.save({fields: ["pregunta", "respuesta", "tema"]})
 		.then( function(){ res.redirect('/quizes')}) ;
 	}
 };
@@ -76,6 +76,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res) {
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
     
     // Validamos el quiz.
     var err = req.quiz.validate();
@@ -89,7 +90,7 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: errores});
     } else {
         req.quiz // Guarda los campos en la BD.
-        .save( {fields: ["pregunta", "respuesta"]} )
+        .save( {fields: ["pregunta", "respuesta", "tema"]} )
         .then( function() { res.redirect('/quizes');});
     }
 };
